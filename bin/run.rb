f1c500fb2cc061bef163bdbe7165e47ff7bdafe9
Carrie Savinell <carrielane89@gmail.com>
2018-05-30 19:00:58 +0000

    automatically backed up by learn

--- bin/run.rb
+++ bin/run.rb
@@ -2,6 +2,8 @@
 
 require_relative '../config/enviornment'
 
+#'require_relative' takes a relative path that is relative to the file in which the require statemnet is called
+
 Hashie
 Sinatra
 Octokit
