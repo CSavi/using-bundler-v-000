e0dd027ca66e0b759fbe249a5676ebeed230d429
Carrie Savinell <carrielane89@gmail.com>
2018-05-30 18:56:42 +0000

    automatically backed up by learn

--- config/environment.rb
+++ config/environment.rb
@@ -1,4 +1,6 @@
 #load all app's dependencies, from gems to database connections
 
 require 'bundler/setup'
-Bundler.require(:default, :development)  #arguments much be written in the correct order
+Bundler.require(:default, :development)  
+
+#arguments much be written in the correct order
